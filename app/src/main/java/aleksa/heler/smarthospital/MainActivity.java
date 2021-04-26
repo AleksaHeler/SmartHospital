303740d8b4ca6f2a50c9082c45310b16316e58fa
Aleksa Heler <aleksaheler@gmail.com>
2021-04-26 08:56:15 +0200

    finished first assignment

--- app/src/main/java/aleksa/heler/smarthospital/MainActivity.java
+++ app/src/main/java/aleksa/heler/smarthospital/MainActivity.java
@@ -2,13 +2,47 @@ package aleksa.heler.smarthospital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText usernameET;
+    EditText passwordET;
+    Button login_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameET = findViewById(R.id.login_username_input);
+        passwordET = findViewById(R.id.login_password_input);
+        login_submit = findViewById(R.id.login_submit_button);
+
+        findViewById(R.id.register_button).setOnClickListener(this);
+        findViewById(R.id.login_button).setOnClickListener(this);
+        login_submit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.register_button){
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            //Intent i = new Intent(this, RegisterActivity.class);
+            //startActivity(i);
+        }
+        else if(v.getId() == R.id.login_button){
+            usernameET.setVisibility(View.VISIBLE);
+            passwordET.setVisibility(View.VISIBLE);
+            login_submit.setVisibility(View.VISIBLE);
+        }
+        else if(v.getId() == R.id.login_submit_button){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 }
--- app/src/main/java/aleksa/heler/smarthospital/RegisterActivity.java
+++ app/src/main/java/aleksa/heler/smarthospital/RegisterActivity.java
@@ -3,12 +3,39 @@ package aleksa.heler.smarthospital;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView ispisTV;
+    EditText imeET;
+    EditText prezimeET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ispisTV = findViewById(R.id.print_label);
+        imeET = findViewById(R.id.name_input);
+        prezimeET = findViewById(R.id.lastname_input);
+
+        findViewById(R.id.submit_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.submit_button){
+            String i = imeET.getText().toString();
+            String p = prezimeET.getText().toString();
+            if(i.isEmpty() || p.isEmpty())
+                return;
+            String s = "Username je:\n";
+            s += i + "." + p;
+            ispisTV.setText(s);
+            ispisTV.setVisibility(View.VISIBLE);
+        }
     }
 }
